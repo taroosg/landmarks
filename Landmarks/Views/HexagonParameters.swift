edd8a0b9015aa4a7edf392fe0fb696f3388b098e
taroosg <root@taroosg.dev>
2023-06-12 17:55:23 +0900

    done section3

--- Landmarks/Views/HexagonParameters.swift
+++ Landmarks/Views/HexagonParameters.swift
@@ -15,7 +15,7 @@ struct HexagonParameters {
   }
   
   static let adjustment: CGFloat = 0.085
-
+  
   static let segments = [
     Segment(
       line:    CGPoint(x: 0.60, y: 0.05),
---
+++ Landmarks/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Taro Ohsugi on 2023/06/12.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+  let angle: Angle
+  
+  var body: some View {
+    BadgeSymbol()
+      .padding(-60)
+      .rotationEffect(angle, anchor: .bottom)
+  }}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+  static var previews: some View {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+  }
+}
