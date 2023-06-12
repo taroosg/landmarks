b3683c145e54c2802b5d0c5405a72f76aae175d3
taroosg <root@taroosg.dev>
2023-06-12 22:25:32 +0900

    done section2

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -17,22 +17,29 @@ struct Landmark: Hashable, Codable, Identifiable {
   var state: String
   var description: String
   var isFavorite: Bool
-
-//  画像関連のプロパティ
+  
+  var category: Category
+  enum Category: String, CaseIterable, Codable {
+    case lakes = "Lakes"
+    case rivers = "Rivers"
+    case mountains = "Mountains"
+  }
+  
+  //  画像関連のプロパティ
   private var imageName: String
   var image: Image {
     Image(imageName)
   }
   
-//  位置情報関連のプロパティ
+  //  位置情報関連のプロパティ
   private var coordinates: Coordinates
-
+  
   var locationCoordinate: CLLocationCoordinate2D {
     CLLocationCoordinate2D(
       latitude: coordinates.latitude,
       longitude: coordinates.longitude)
   }
-
+  
   struct Coordinates: Hashable, Codable {
     var latitude: Double
     var longitude: Double
--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -8,16 +8,23 @@
 import SwiftUI
 
 struct CategoryHome: View {
-    var body: some View {
-      NavigationView {
-          Text("Hello, World!")
-          .navigationTitle("Featured")
+  @EnvironmentObject var modelData: ModelData
+  
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+          Text(key)
+        }
       }
+      .navigationTitle("Featured")
     }
+  }
 }
 
 struct CategoryHome_Previews: PreviewProvider {
-    static var previews: some View {
-        CategoryHome()
-    }
+  static var previews: some View {
+    CategoryHome()
+      .environmentObject(ModelData())
+  }
 }
