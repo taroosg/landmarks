af4e8a71f694e91dda99e497740edf693eccf0b1
taroosg <root@taroosg.dev>
2023-06-12 22:44:03 +0900

    done section5

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-  var body: some View {
-    LandmarkList()
+  @State private var selection: Tab = .featured
+  
+  enum Tab {
+    case featured
+    case list
   }
+  
+  var body: some View {
+    TabView(selection: $selection) {
+      CategoryHome()
+        .tabItem {
+          Label("Featured", systemImage: "star")
+        }
+        .tag(Tab.featured)
+      
+      
+      LandmarkList()
+        .tabItem {
+          Label("List", systemImage: "list.bullet")
+        }
+        .tag(Tab.list)
+    }  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-      ContentView()
-        .environmentObject(ModelData())
-    }
+  static var previews: some View {
+    ContentView()
+      .environmentObject(ModelData())
+  }
 }
